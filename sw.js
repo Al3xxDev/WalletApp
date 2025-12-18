e3509f9756a31c5cee827c7f902a12c1bfb834b5
Alessandro Barbato <al3xx.iphone@icloud.com>
2025-12-18 15:00:42 +0100

    modified "icon.png" to "icon.svg"

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "wallet-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.png"];
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
